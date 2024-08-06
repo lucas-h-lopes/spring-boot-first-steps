d7a336a111193b690940b63381680d67694340e9
Lucas <lucas.hen.lps@gmail.com>
2024-08-06 15:49:13 -0300

    feat: OrderItemPK class

---
+++ src/main/java/com/study/springboot_project/entities/pk/OrderItemPK.java
@@ -0,0 +1,34 @@
+package com.study.springboot_project.entities.pk;
+
+import com.study.springboot_project.entities.Order;
+import com.study.springboot_project.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class OrderItemPK {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
