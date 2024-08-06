4434ba925029eb238105875dbe0b3dba9d40408c
Lucas <lucas.hen.lps@gmail.com>
2024-08-06 15:38:44 -0300

    feat: validation to Customer fields

--- src/main/java/com/study/springboot_project/entities/Customer.java
+++ src/main/java/com/study/springboot_project/entities/Customer.java
@@ -1,32 +1,37 @@
 package com.study.springboot_project.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @NoArgsConstructor
+@Table(name = "tb_customer")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "Name: Can't be null or blank")
+    @Size(min = 3, max = 30, message = "Name: Must be 3 to 30 characters")
     private String name;
+    @NotBlank(message = "Email: Can't be null or blank")
+    @Email(message = "Email: Invalid email format")
     private String email;
 
     @OneToMany(mappedBy = "customer")
-    private Set<Product> products = new HashSet<>();
+    private Set<Order> orders = new HashSet<>();
 
     public Customer(String name, String email){
         this.name = name;
         this.email = email;
-    }
-
-    public Set<Product> getProducts(){
-        return products;
     }
 
     public Long getId() {
@@ -47,5 +52,18 @@ public class Customer {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
