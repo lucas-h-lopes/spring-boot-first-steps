526a0663d05f661334d0de40e7ce21de97fb5180
Lucas <lucas.hen.lps@gmail.com>
2024-08-06 15:41:24 -0300

    feat: new Product repository method: findByName

--- src/main/java/com/study/springboot_project/repositories/ProductRepository.java
+++ src/main/java/com/study/springboot_project/repositories/ProductRepository.java
@@ -4,4 +4,5 @@ import com.study.springboot_project.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByNameIgnoreCase(String name);
 }
