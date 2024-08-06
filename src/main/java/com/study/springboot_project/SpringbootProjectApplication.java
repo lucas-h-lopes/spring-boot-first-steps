f943254b8432e82e75895c80fca62cf8f305caa8
Lucas <lucas.hen.lps@gmail.com>
2024-08-06 08:12:34 -0300

    feat: initial setup

---
+++ src/main/java/com/study/springboot_project/SpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/springboot_project/SpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
